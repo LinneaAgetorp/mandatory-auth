c2e9786000f5fbe8c0596c9cc8b961321d4e3425
linneaagetorp <linnea.agetorp@gmail.com>
2018-05-24 13:20:14 +0200

    fixes...

--- react/src/App.js
+++ react/src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
             })
             .then(() => {
                 this.setState(AuthService.getAuthState())
+                this.loginFail = false
             })
 
     };
